ed8598d279665dddcc82f997a15238e26827c602
Austin Chen <akrolsmir@gmail.com>
2012-06-26 21:05:29 -0700

    ModTrace now starts without prompting

--- src/modtrace/ModTrace.java
+++ src/modtrace/ModTrace.java
@@ -3,14 +3,13 @@ package modtrace;
 import java.io.File;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import javax.swing.JOptionPane;
 
-import tabs.AIBatTab;
 import tabs.AIBatTabbedPane;
 import tabs.ModTraceTab;
-
 import aibat.OsuFileParser;
 
 public class ModTrace implements Runnable {
@@ -22,10 +21,9 @@ public class ModTrace implements Runnable {
     private Map<OsuFileParser, File> parsedOrigFiles;
 
     public ModTrace(List<File> osuFiles, AIBatTabbedPane tabs) {
-	// this.osuFiles = osuFiles;
-
-	if (!askToStart())
-	    return;
+	// No longer asks to start, just does it.
+	// if (!askToStart())
+	// return;
 
 	OsuFilesCopier ofc = new OsuFilesCopier(osuFiles);
 	parsedOrigFiles = ofc.getParsedOrigFiles();
@@ -36,19 +34,19 @@ public class ModTrace implements Runnable {
     }
 
     // Asks if the user wants to start ModTrace
-    private boolean askToStart() {
-	int n = JOptionPane.showConfirmDialog(null, "Would you like to start "
-		+ VERSION + "?\n\n"
-		+ "(ModTrace is an experimental function of AIBat that \n"
-		+ "automatically tracks changes saved from the .osu editor.)",
-		"Start ModTrace?", JOptionPane.YES_NO_OPTION);
-	return n == 0;
-    }
+    // private boolean askToStart() {
+    // int n = JOptionPane.showConfirmDialog(null, "Would you like to start "
+    // + VERSION + "?\n\n"
+    // + "(ModTrace is an experimental function of AIBat that \n"
+    // + "automatically tracks changes saved from the .osu editor.)",
+    // "Start ModTrace?", JOptionPane.YES_NO_OPTION);
+    // return n == 0;
+    // }
 
     // Runs a DiffComparator for each pair and maps titles to changes
     public Map<String, String> compareAll() {
 	TreeMap<String, String> result = new TreeMap<String, String>();
-	for (Map.Entry<OsuFileParser, File> entry : parsedOrigFiles.entrySet()) {
+	for (Entry<OsuFileParser, File> entry : parsedOrigFiles.entrySet()) {
 	    DiffComparator d = new DiffComparator(entry.getKey(),
 		    new OsuFileParser(entry.getValue()));
 	    result.put(entry.getKey().getDiff(), d.compare());
